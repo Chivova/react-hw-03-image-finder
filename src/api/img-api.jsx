f819d61205c3a44745dd7b6c995512f56338b174
Chivova <Chivovaz@ua.fm>
2021-06-15 16:15:22 +0300

    add comp Button, partial execution hw

--- src/api/img-api.jsx
+++ src/api/img-api.jsx
@@ -3,15 +3,13 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '21510937-25dddef59cfdf7f1b5603f7e9';
 
-const fetchImgApi = query => {
+const fetchImgApi = (query, page) => {
   return axios
     .get(
-      `${BASE_URL}/?q=${query}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+      `${BASE_URL}/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
     )
     .then(r => r.data.hits)
-    .catch(function (error) {
-      console.log(JSON.stringify(error));
-    });
+    .catch(function (error) {});
 };
 
 const imgApi = {
--- src/components/SearchInfo/SearchInfo.jsx
+++ src/components/SearchInfo/SearchInfo.jsx
@@ -1,22 +1,28 @@
 import { Component } from 'react';
 import imgApi from '../../api/img-api';
 import ImageGallery from '../ImageGallery';
+import Button from '../Button';
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 class SearchInfo extends Component {
   state = {
     gallery: [],
+    page: 1,
     loading: false,
     error: null,
   };
   componentDidUpdate(prevProps, prevState) {
+    const { page } = this.state;
     const prevQuery = prevProps.imageQuery;
     const nextQuery = this.props.imageQuery;
+    console.log(prevState);
 
     if (prevQuery !== nextQuery) {
       this.setState({ loading: true, gallery: [] });
 
       imgApi
-        .fetchImgApi(nextQuery)
+        .fetchImgApi(nextQuery, page)
         .then(gallery => {
           if (gallery.length === 0) {
             // return Promise.reject(error.response);
@@ -24,7 +30,10 @@ class SearchInfo extends Component {
               error: `No images found on your request ${nextQuery}`,
             });
           }
-          this.setState({ gallery });
+          this.setState(prevState => ({
+            gallery: [...prevState.gallery, ...gallery],
+            page: page + 1,
+          }));
         })
         .finally(() => this.setState({ loading: false }));
     }
@@ -36,8 +45,11 @@ class SearchInfo extends Component {
     return (
       <div>
         {error && <h2>{error}</h2>}
-        {loading && <p>Load......</p>}
+        {loading && (
+          <Loader type="Puff" color="#00BFFF" height={50} width={50} />
+        )}
         <ImageGallery gallery={gallery} />
+        <Button />
       </div>
     );
   }
